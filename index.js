1f39a35752e98be04d683ab569f74cfdea8076b5
Hunter Parmley <parmleyhunt@gmail.com>
2020-06-19 20:24:26 -0500

    Fixed global issue

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 global.__root = __dirname
-global.__lib = __dirname+"/lib"
-gloabl.__pants = __dirname+"/pants"
-gloabl.__fonts = __dirname+"/fonts"
-gloabl.__domains = __dirname+"/domains"
+global.__lib = __dirname+"/lib/"
+global.__pants = __dirname+"/pants/"
+global.__fonts = __dirname+"/fonts/"
+global.__domains = __dirname+"/domains/"
 
 let app = require('./lib/')
